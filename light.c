e5724897d43b9758dd886ece28115c36bc9241b4
kookjd7759 <kookjd7759@naver.com>
2026-01-07 11:16:31 +0900

    add light struct

---
+++ light.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+
+typedef enum {
+    GO_ST = 1, // green
+    WAIT,      // yelow
+    STOP,      // red
+    GO_LEFT    // arrow
+} STATE;
+const char st_chr[5][30] = { // state to string function
+    "",
+    " ¡Ü ¡Ü ¡ç \033[32m ¡Ü \033[0m",
+    " ¡Ü \033[33m ¡Ü \033[0m ¡ç ¡Ü",
+    "\033[31m ¡Ü \033[0m ¡Ü ¡ç ¡Ü",
+    " ¡Ü ¡Ü \033[32m ¡ç \033[0m ¡Ü"
+};
+
+// main light struct
+typedef struct {
+    int id;
+    char name[10];
+    STATE state;
+
+} Light;
+
+void print_light(const Light* light) {
+    printf("[%d] %s - [%s]", light->id, light->name, st_chr[light->state]);
+}
+
+void change_light(Light* light, const STATE* state) {
+    light->state = *state;
+}
+
+int main() {
+    printf("[test]\n");
+    Light light = { 0, "basic", GO_ST };
+    print_light(&light);
+}
