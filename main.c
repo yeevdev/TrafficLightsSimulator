e765ef16d8d9aef68df79d1a06f2b0e17decc804
cenway <yhj02021212@gmail.com>
2026-01-07 11:27:50 +0900

    메인

--- main.c
+++ main.c
@@ -1,8 +1,55 @@
 ﻿#include <stdio.h>
+#include <windows.h>
+#include ""
+
+//상태가 n개 
+enum state_t
+{
+	S0,
+	S1,
+	S2,
+	S3,
+	S4,
+	S5,
+	S6,
+	S7
+};
+
+//각 상태에 맞는 신호등 상태
 
 
-int main(void) {
+int main(void)
+{
+	//초기화
 
-
+	//루프
+	while (1)
+	{
+		state_t cur_state = S0;
+		//호출
+		Sleep(40000);//40s
+		cur_state = S1;
+		//호출
+		Sleep(6000);//6s
+		cur_state = S2;
+		//호출
+		Sleep(60000);//60s
+		cur_state = S3;
+		//호출
+		Sleep(6000);//6s
+		cur_state = S4;
+		//호출
+		Sleep(40000);//40s
+		cur_state = S5;
+		//호출
+		Sleep(6000);//6s
+		cur_state = S6;
+		//호출
+		Sleep(60000);//60s
+		cur_state = S7;
+		//호출
+		Sleep(6000);//6s
+	}
 	return 0;
 }
+
