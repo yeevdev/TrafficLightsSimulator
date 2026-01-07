58538f436339cec2b0f4c1e2b04aa35783b750cd
cenway <yhj02021212@gmail.com>
2026-01-07 11:57:58 +0900

    main 비동기

--- main.c
+++ main.c
@@ -1,9 +1,9 @@
 ﻿#include <stdio.h>
 #include <windows.h>
-#include ""
 
-//상태가 n개 
-enum state_t
+
+//상태가 n개
+typedef enum
 {
 	S0,
 	S1,
@@ -13,7 +13,7 @@ enum state_t
 	S5,
 	S6,
 	S7
-};
+}state_t;
 
 //각 상태에 맞는 신호등 상태
 
@@ -21,34 +21,82 @@ enum state_t
 int main(void)
 {
 	//초기화
-
+    ULONGLONG last_tick = GetTickCount64();
+    state_t cur_state = S0;
+    unsigned long long wait_time = 4000;
+    printf("0");
 	//루프
 	while (1)
 	{
-		state_t cur_state = S0;
-		//호출
-		Sleep(40000);//40s
-		cur_state = S1;
-		//호출
-		Sleep(6000);//6s
-		cur_state = S2;
-		//호출
-		Sleep(60000);//60s
-		cur_state = S3;
-		//호출
-		Sleep(6000);//6s
-		cur_state = S4;
-		//호출
-		Sleep(40000);//40s
-		cur_state = S5;
-		//호출
-		Sleep(6000);//6s
-		cur_state = S6;
-		//호출
-		Sleep(60000);//60s
-		cur_state = S7;
-		//호출
-		Sleep(6000);//6s
+        ULONGLONG current_tick = GetTickCount64();
+		
+        
+        // 2. 시간 비교 (현재시간 - 마지막변경시간 >= 대기시간)
+        if (current_tick - last_tick >= wait_time)
+        {
+            // 기준 시간(last_tick)을 현재로 갱신 (타이머 리셋 효과)
+            last_tick = current_tick;
+
+            // 상태 전이 로직
+            switch (cur_state)
+            {
+            case S0:
+                cur_state = S1;
+                wait_time = 600; // 6s
+                printf("1");
+                // [호출]
+                break;
+
+            case S1:
+                cur_state = S2;
+                wait_time = 6000; // 60s
+                printf("2");
+                // [호출]
+                break;
+
+            case S2:
+                cur_state = S3;
+                wait_time = 600; // 6s
+                printf("3");
+                // [호출]
+                break;
+
+            case S3:
+                cur_state = S4;
+                wait_time = 4000; // 40s
+                printf("4");
+                // [호출]
+                break;
+
+            case S4:
+                cur_state = S5;
+                wait_time = 600; // 6s
+                printf("5");
+                // [호출]
+                break;
+
+            case S5:
+                cur_state = S6;
+                wait_time = 6000; // 60s
+                printf("6");
+                // [호출]
+                break;
+
+            case S6:
+                cur_state = S7;
+                wait_time = 600; // 6s
+                printf("7");
+                // [호출]
+                break;
+
+            case S7:
+                cur_state = S0;
+                wait_time = 4000; // 40s (처음 S0 시간으로 돌아감)
+                printf("0");
+                // [호출]
+                break;
+            }
+        }
 	}
 	return 0;
 }
