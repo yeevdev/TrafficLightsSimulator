e358c109e29b65a60b95950c27653ea25d04d4a5
kookjd7759 <kookjd7759@naver.com>
2026-01-07 11:23:16 +0900

    sep

--- light.c
+++ light.c
@@ -1,26 +1,4 @@
-#include <stdio.h>
-
-typedef enum {
-    GO_ST = 1, // green
-    WAIT,      // yelow
-    STOP,      // red
-    GO_LEFT    // arrow
-} STATE;
-const char st_chr[5][30] = { // state to string function
-    "",
-    " ¡Ü ¡Ü ¡ç \033[32m ¡Ü \033[0m",
-    " ¡Ü \033[33m ¡Ü \033[0m ¡ç ¡Ü",
-    "\033[31m ¡Ü \033[0m ¡Ü ¡ç ¡Ü",
-    " ¡Ü ¡Ü \033[32m ¡ç \033[0m ¡Ü"
-};
-
-// main light struct
-typedef struct {
-    int id;
-    char name[10];
-    STATE state;
-
-} Light;
+#include "light.h"
 
 void print_light(const Light* light) {
     printf("[%d] %s - [%s]", light->id, light->name, st_chr[light->state]);
